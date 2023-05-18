e8e044e9ccb7c0ccf90773d8e48c7ba28faaa2f0
Rishita-Mavani <rishitab03@gmail.com>
2023-05-18 16:29:45 +0530

    created component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import ProductList from './Pages/ProductList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/login' Component={Login} />
+            <Route path='/register' Component={Register} />
+            <Route path='/product-list' Component={ProductList} />
+            <Route path='/' Component={Login} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+function Login(){
+    return(
+        <>
+            <Header />
+            <h1>Login Component</h1><br />
+
+            <Link to ="/Register">Register</Link><br />
+            <Link to ="product-list">Login</Link>
+            <Footer />
+        </>
+    );
+}
+
+export default Login;
