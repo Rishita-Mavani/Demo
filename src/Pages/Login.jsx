d814e3a6b9c5097fadfd0f6b69f4635d002fc0cb
Rishita-Mavani <rishitab03@gmail.com>
2023-05-21 18:30:06 +0530

    header,footer and searchbar

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-// import { Button, TextField, styled } from "@mui/material";
+import Searchbar from "../components/Searchbar";
+import { Button, TextField, styled } from "@mui/material";
 
 function Login() {
     return (
         <>
         <Header />
+        <Searchbar />
+        <Button variant="contained">Log In</Button>&nbsp;&nbsp;
+        <Button variant="outlined">Cancel</Button><br />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
             <h1>Login or Create an account</h1><br />
 
             <Link to="/Register">Register</Link><br />
