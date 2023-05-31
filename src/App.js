6b3958f72ed85ad72f519caf264f4ce86533c957
Rishita-Mavani <rishitab03@gmail.com>
2023-05-31 16:00:20 +0530

    Day 8

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Pages/Login';
-import Register from './Pages/Register';
 import ProductList from './Pages/ProductList';
+import Register from './Pages/Register';
+import ProductEdit from './Pages/ProductEdit';
+import Cart from './pages/Cart';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import Footer from './components/Footer'
+
+// import { ThemeProvider, createTheme } from '@mui/material';
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
+      {/* <ThemeProvider> */}
+      <ToastContainer theme="colored"/>
+        <BrowserRouter>
+        <Header />
+          <Routes>
             <Route path='/login' Component={Login} />
             <Route path='/register' Component={Register} />
             <Route path='/product-list' Component={ProductList} />
+            <Route path='/product-edit' Component={ProductEdit} />
+            <Route path='/cart' Component={Cart} />
             <Route path='/' Component={Login} />
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        <Footer />
+        </BrowserRouter>
+      {/* </ThemeProvider> */}
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { addIcon } from '../assets/images';
+import React from "react";
+import { addIcon } from "../assets/images";
 
 function Footer(){
     return(
         <>
         <div className='footeralign'>
-            { <img src={addIcon} className='footerimg' alt="" /> }
+            { <img src={ addIcon } className='footerimg' alt="" /> }
+            <p className="copyright-text">@ 2015 Tatvasoft.com. All rights reserved.</p>
         </div>
         </>
-    );
+    )
 }
 
 export default Footer;
