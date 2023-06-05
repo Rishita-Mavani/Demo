725afe728cdfa46dca02f02d0e8fa43e13521bc1
Rishita-Mavani <rishitab03@gmail.com>
2023-06-05 18:02:12 +0530

    Task 9,10 & 11

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './Pages/Login';
-import ProductList from './Pages/ProductList';
-import Register from './Pages/Register';
-import ProductEdit from './Pages/ProductEdit';
-import Cart from './pages/Cart';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Header from './components/Header';
-import Footer from './components/Footer'
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./utils/theme";
+import "./assets/css/style.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
+import { MainNavigation } from "./components/MainNavigation";
+// import loader from "../src/assets/images/loader.gif";
+import { Footer } from "./components/footer";
+import { AuthWrapper } from "./context/auth";
+import Header from "./components/header";
 
-// import { ThemeProvider, createTheme } from '@mui/material';
-
-
-function App() {
+const App = () => {
   return (
-    <>
-      {/* <ThemeProvider> */}
-      <ToastContainer theme="colored"/>
-        <BrowserRouter>
-        <Header />
-          <Routes>
-            <Route path='/login' Component={Login} />
-            <Route path='/register' Component={Register} />
-            <Route path='/product-list' Component={ProductList} />
-            <Route path='/product-edit' Component={ProductEdit} />
-            <Route path='/cart' Component={Cart} />
-            <Route path='/' Component={Login} />
-          </Routes>
-        <Footer />
-        </BrowserRouter>
-      {/* </ThemeProvider> */}
-    </>
+    <BrowserRouter>
+      <AuthWrapper>
+        <ThemeProvider theme={theme}>
+          {/* <div className="loader-wrapper">
+            <img src={loader} alt="loader" />
+          </div> */}
+          <Header />
+          <main>
+            <MainNavigation />
+          </main>
+          <Footer />
+          <ToastContainer />
+        </ThemeProvider>
+      </AuthWrapper>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
